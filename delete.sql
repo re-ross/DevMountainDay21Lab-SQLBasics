cbdf16cd3aba1ba9a9e5c678657b00b2be1894ca
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-31 16:11:25 -0600

    finished lab

--- delete.sql
+++ delete.sql
@@ -1,3 +1,10 @@
+CREATE TABLE animals (
+  name VARCHAR(30),
+  type VARCHAR(30),
+  age INTEGER
+  );
+
+
 -- INSERT INTO animals ( name, type, age ) 
 -- VALUES ('Leo', 'lion', 12),
 -- ('Jerry', 'mouse', 4),
@@ -8,3 +15,12 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+DELETE FROM animals
+WHERE type = 'lion';
+
+DELETE FROM animals
+WHERE name ILIKE ('M%');
+
+DELETE FROM animals
+WHERE age > 9;
